2bce0bad1bd16bc4e10eea705412749d0f1b0220
Rokibul Islam <rokib.hridoy@gmail.com>
2024-05-08 11:58:56 +0600

    chore: Update font family and colors in UI components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,50 +1,52 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+
   theme: {
     extend: {
       colors: {
         primary: {
-          200: "#97D4C7",
-          600: "#05AA85",
+          200: '#97D4C7',
+          600: '#05AA85',
         },
         accent: {
-          200: "#4B648B",
-          400: "#1D3D70",
-          600: "#162C4E",
+          200: '#4B648B',
+          400: '#1D3D70',
+          600: '#162C4E',
         },
-        secondary: "#BEBEBE",
-        dark: "#222",
-        light: "#F5F5F5",
+        secondary: '#828282',
+        dark: '#222',
+        light: '#f4f7f3',
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-        poppins: ["Poppins", "sans-serif"],
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
       },
       fontWeight: {
-        thin: "100",
-        extralight: "200",
-        light: "300",
-        regular: "400",
-        medium: "500",
-        semibold: "600",
-        bold: "700",
-        extrabold: "800",
-        black: "900",
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
       },
       fontStyle: {
-        normal: "normal",
-        italic: "italic",
+        normal: 'normal',
+        italic: 'italic',
       },
     },
   },
